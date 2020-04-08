033a6615bf7c06fca68d58ceacfb7201cb730091
rushkock <ruchella_kock@hotmail.com>
2020-04-09 00:06:17 +0200

    final code

---
+++ static/js/bundle.js
@@ -0,0 +1,34 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+// This function will load the data in an order
+// ONLY when the data is loaded then it will execute a function
+// In this function we call the functions to make the D3 visualizations
+  const fs = require('fs')
+const customer = {
+    name: "Newbie Co.",
+    order_count: 0,
+    address: "Po Box City",
+}
+const jsonString = JSON.stringify(customer)
+fs.writeFile('../static/data/newCustomer.json', jsonString, err => {
+    if (err) {
+        console.log('Error writing file', err)
+    } else {
+        console.log('Successfully wrote file')
+    }
+})
+
+
+
+// this fun
+// 1 GB
+//60000000
+// 500 mb
+//30000000
+// 250 mb
+// 15000000
+//1.048.576.000
+//1.073.741.824
+
+},{"fs":2}],2:[function(require,module,exports){
+
+},{}]},{},[1]);
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,10 @@
-// This function will load the data in an order
-// ONLY when the data is loaded then it will execute a function
-// In this function we call the functions to make the D3 visualizations
-window.onload = function()
-{
-  const size_of_buffer = 1*1024*1024
-  var buffer = new ArrayBuffer(size_of_buffer)
 
+function loadData()
+{
+  const size_of_buffer = 2*1024*1024*1024
+  var buffer = new ArrayBuffer(size_of_buffer)
   var data = new Uint32Array(buffer, 0, size_of_buffer/2/Uint32Array.BYTES_PER_ELEMENT)
   var data2 = new Uint32Array(buffer, size_of_buffer/2)
-
   for (let i=0; i< data.length; i++){
     data[i] = i
     data2[i] = Math.random() * 100
@@ -16,11 +12,62 @@ window.onload = function()
   makeBar(data, data2)
 };
 
-// 1 GB
-//60000000
-// 500 mb
-//30000000
-// 250 mb
-// 15000000
-//1.048.576.000
-//1.073.741.824
+// read csv
+function read_csv(){
+var requests = [d3.csv('../static/data/big_data_1.csv')];
+
+Promise.all(requests).then(function(response) {
+   console.log(response)
+   console.log(roughSizeOfObject(response))
+   console.log(JSON.stringify(response).length)
+}).catch(function(e) {
+    throw(e);
+});
+};
+function roughSizeOfObject( object ) {
+
+    var objectList = [];
+    var stack = [ object ];
+    var bytes = 0;
+
+    while ( stack.length ) {
+        var value = stack.pop();
+
+        if ( typeof value === 'boolean' ) {
+            bytes += 4;
+        }
+        else if ( typeof value === 'string' ) {
+            bytes += value.length * 2;
+        }
+        else if ( typeof value === 'number' ) {
+            bytes += 8;
+        }
+        else if
+        (
+            typeof value === 'object'
+            && objectList.indexOf( value ) === -1
+        )
+        {
+            objectList.push( value );
+
+            for( var i in value ) {
+                stack.push( value[ i ] );
+            }
+        }
+    }
+    return bytes;
+}
+// read json
+function read_json(){
+data = []
+oboe('../static/data/big_data_2.json')
+   .node('*', function( result ){
+		 data.push(result)
+ })
+  .done(function(result){
+  	console.log(data)
+  })
+	.fail(function(error) {
+		 console.log(error)
+	});
+};
